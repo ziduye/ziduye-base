f9b6dc49d5e17fdadc0269c59f1916dd983f4e2b
ziduye <ziduye@gmail.com>
2016-06-01 00:52:45 +0800

    2016年5月29日01:25:22

--- manage/src/main/java/com/ziduye/modules/sys/entity/Role.java
+++ manage/src/main/java/com/ziduye/modules/sys/entity/Role.java
@@ -1,37 +1,43 @@
 package com.ziduye.modules.sys.entity;
 
-import java.util.List;
-
 import com.google.common.collect.Lists;
 import com.ziduye.base.entity.DataEntity;
 
+import java.util.List;
+
 public class Role extends DataEntity<Role> {
 	
-	private String name;
-	private String pid;
-	private String pids;
+	private String roleSN;
+	private String roleName;
+	private String roleType;
 	
 	private List<Menu> menuList = Lists.newArrayList();
-	
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getPid() {
-		return pid;
-	}
-	public void setPid(String pid) {
-		this.pid = pid;
-	}
-	public String getPids() {
-		return pids;
-	}
-	public void setPids(String pids) {
-		this.pids = pids;
-	}
-	public List<Menu> getMenuList() {
+
+    public String getRoleSN() {
+        return roleSN;
+    }
+
+    public void setRoleSN(String roleSN) {
+        this.roleSN = roleSN;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public String getRoleType() {
+        return roleType;
+    }
+
+    public void setRoleType(String roleType) {
+        this.roleType = roleType;
+    }
+
+    public List<Menu> getMenuList() {
 		return menuList;
 	}
 	public void setMenuList(List<Menu> menuList) {
